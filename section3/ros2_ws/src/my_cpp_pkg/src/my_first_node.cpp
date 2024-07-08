c14c325db3b8a4f81997c10b588bc407a82bc4eb
farhan-sw <farhansw.off@gmail.com>
2024-07-08 09:21:02 +0700

    section3-simple-cpp-with-oop

--- section3/ros2_ws/src/my_cpp_pkg/src/my_first_node.cpp
+++ section3/ros2_ws/src/my_cpp_pkg/src/my_first_node.cpp
@@ -1,16 +1,34 @@
 #include <rclcpp/rclcpp.hpp>
 
-int main(int args, char **argv) {
+class MyNode : public rclcpp::Node
+{
+public:
+    MyNode() : Node("cpp_test"), counter_(0)
+    {
+        RCLCPP_INFO(this->get_logger(), "Hello Cpp Node");
+        timer_ = this->create_wall_timer(std::chrono::seconds(1),
+                                         std::bind(&MyNode::timerCallback, this));
+    }
+
+private:
+    rclcpp::TimerBase::SharedPtr timer_;
+    int counter_ = 0;
+
+    void timerCallback()
+    {
+        counter_++;
+        RCLCPP_INFO(this->get_logger(), "Hello %d", counter_);
+    }
+};
+
+int main(int args, char **argv)
+{
     // Initialize the ROS 2 system
     rclcpp::init(args, argv);
 
-
     // Create a node
-    auto node = std::make_shared<rclcpp::Node>("cpp_test");
-    RCLCPP_INFO(node->get_logger(), "Hello Cpp Node");
-
+    auto node = std::make_shared<MyNode>();
     rclcpp::spin(node);
-
 
     // Shutdown the ROS 2 system
     rclcpp::shutdown();
